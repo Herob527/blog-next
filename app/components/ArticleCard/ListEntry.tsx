c93ff9a5152b45cbe5f1938ef11baf1cc410ad16
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-10-22 20:53:32 +0200

    Added sizes

--- app/components/ArticleCard/ListEntry.tsx
+++ app/components/ArticleCard/ListEntry.tsx
@@ -28,7 +28,14 @@ const ListEntryImage = ({ title, file, alt }: propsImage) => {
   return (
     <li className="inline-flex relative flex-col flex-1 items-center text-center">
       <div className="relative mb-2 w-12 h-12">
-        <Image src={file} fill alt={alt} className="object-contain" priority />
+        <Image
+          src={file}
+          fill
+          alt={alt}
+          className="object-contain"
+          priority
+          sizes="(max-width: 768px) 100vw, 33vw"
+        />
       </div>
       <span>{title}</span>
     </li>
