ac649f8e47b5de86e2b7af2276104af8918de7d7
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-11-05 22:07:14 +0100

    Added onLostFocus, changed onClick to onFocus and fixed button being out of the line and made it responsive

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -4,41 +4,48 @@ type Props<T> = {
   data: T[];
   keyFunc: (data: T) => string;
   labelFunc?: (data: T) => string;
-  onClick: (data: T) => void;
+  onFocus: (data: T) => void;
+  onLostFocus: (data: T) => void
 };
 
 export function TimeLine<T = unknown>({
   data,
   keyFunc,
   labelFunc,
-  onClick,
+  onFocus,
+  onLostFocus
 }: Props<T>) {
   const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
 
   const handleClick = (index: number) => {
-    onClick(data[index]);
-    setSelectedIndex(index);
+    const currentlySelected = selectedIndex === index ? null : index
+    if (currentlySelected !== null) {
+      onFocus(data[index]);
+    } else {
+      onLostFocus(data[index])
+    }
+    setSelectedIndex(currentlySelected);
   };
   console.log("[TimeLine - label]", labelFunc);
   const generateKey = (data: T) => keyFunc(data);
 
   return (
     <>
-      <div className="flex relative flex-row flex-nowrap justify-around items-center mb-8 w-full">
-        <span className="absolute z-0 w-full h-1 border-t-2 border-b-2 border-blue-500"></span>
+      <div className="flex relative flex-col flex-nowrap gap-4 justify-around items-start mb-8 w-full md:flex-row md:items-center">
+        <span className="absolute z-0 w-0 h-[calc(100%+1rem)] border-l-4 border-blue-500 md:w-full md:h-1 md:border-0 md:border-r-0 md:border-t-4"></span>
         {data.map((val, index) => (
           <button
             key={`timeline-${generateKey(val)}`}
             onClick={() => handleClick(index)}
-            className="inline-flex z-10 flex-col flex-nowrap items-center sm:translate-y-[16px]"
+            className="inline-flex z-10 flex-row flex-nowrap gap-4 items-start md:flex-col md:gap-0 md:items-center"
           >
+            <span className="hidden md:block">&nbsp;</span>
             <span
               key={`button-timeline-${generateKey(val)}`}
-              className={`inline-block p-2 rounded-full border-4 ${
-                selectedIndex === index
-                  ? "bg-blue-700 border-blue-500"
-                  : "bg-white border-blue-500"
-              } transition-all duration-500 cursor-pointer `}
+              className={`inline-block p-2 rounded-full border-4 ${selectedIndex === index
+                ? "bg-blue-700 border-blue-500"
+                : "bg-white border-blue-500"
+                } transition-all duration-500 cursor-pointer -translate-x-[calc(50%-2px)] md:translate-x-0`}
             ></span>
             <span className="whitespace-nowrap">
               {labelFunc ? labelFunc(val) : ""}
