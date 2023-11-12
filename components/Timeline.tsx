b5a0a1ccd77c3b5040ec278a67036e52507890ac
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-11-12 19:00:58 +0100

    Added onLostFocus callback

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -5,7 +5,7 @@ type Props<T> = {
   keyFunc: (data: T) => string;
   labelFunc?: (data: T) => string;
   onFocus: (data: T) => void;
-  onLostFocus: (data: T) => void
+  onLostFocus: (data: T) => void;
 };
 
 export function TimeLine<T = unknown>({
@@ -13,16 +13,16 @@ export function TimeLine<T = unknown>({
   keyFunc,
   labelFunc,
   onFocus,
-  onLostFocus
+  onLostFocus,
 }: Props<T>) {
   const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
 
   const handleClick = (index: number) => {
-    const currentlySelected = selectedIndex === index ? null : index
+    const currentlySelected = selectedIndex === index ? null : index;
     if (currentlySelected !== null) {
       onFocus(data[index]);
     } else {
-      onLostFocus(data[index])
+      onLostFocus(data[index]);
     }
     setSelectedIndex(currentlySelected);
   };
@@ -32,7 +32,7 @@ export function TimeLine<T = unknown>({
   return (
     <>
       <div className="flex relative flex-col flex-nowrap gap-4 justify-around items-start mb-8 w-full md:flex-row md:items-center">
-        <span className="absolute z-0 w-0 h-[calc(100%+1rem)] border-l-4 border-blue-500 md:w-full md:h-1 md:border-0 md:border-r-0 md:border-t-4"></span>
+        <span className="absolute z-0 w-0 h-[calc(100%+1rem)] border-l-4 border-mustard-300 md:w-full md:h-1 md:border-0 md:border-r-0 md:border-t-4"></span>
         {data.map((val, index) => (
           <button
             key={`timeline-${generateKey(val)}`}
@@ -42,10 +42,11 @@ export function TimeLine<T = unknown>({
             <span className="hidden md:block">&nbsp;</span>
             <span
               key={`button-timeline-${generateKey(val)}`}
-              className={`inline-block p-2 rounded-full border-4 ${selectedIndex === index
-                ? "bg-blue-700 border-blue-500"
-                : "bg-white border-blue-500"
-                } transition-all duration-500 cursor-pointer -translate-x-[calc(50%-2px)] md:translate-x-0`}
+              className={`inline-block p-2 rounded-full border-4 ${
+                selectedIndex === index
+                  ? "bg-mustard-700 border-mustard-300"
+                  : "bg-white border-mustard-300"
+              } transition-all duration-500 cursor-pointer -translate-x-[calc(50%-2px)] md:translate-x-0`}
             ></span>
             <span className="whitespace-nowrap">
               {labelFunc ? labelFunc(val) : ""}
